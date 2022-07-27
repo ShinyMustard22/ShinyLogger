49510c1d100ebcbb2fe72a12b5f7cffec4963246
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-07-26 22:37:03 -0700

    Added a prompt for when there are no ongoing hunts

--- pages/home/hunting.jsx
+++ pages/home/hunting.jsx
@@ -1,10 +1,27 @@
 import HomeLayout from "../../layouts/HomeLayout";
+import AddHunt from "../../components/AddHuntMenu";
+import styles from "../../styles/Hunting.module.css";
+import { useState } from "react";
 
 export default function Hunting() {
+    const [addHuntVis, setaddHuntVis] = useState(false);
+
     return (
         <>
+            {noHunts()}
+            {addHuntVis ? <AddHunt className={styles.addhuntcontainer}/> : null}
         </>
     );
+
+    function noHunts() {
+        return (
+            <div className={styles.container}>
+                <h1>You have no current hunts...</h1>
+                <h2>Click below to start hunting!</h2>
+                <div className={styles.addpokemon} onClick={() => setaddHuntVis(!addHuntVis)}>+</div>
+            </div>
+        )
+    }
 }
 
 Hunting.Layout = HomeLayout;
