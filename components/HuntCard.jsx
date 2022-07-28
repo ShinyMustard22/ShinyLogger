88e2b47c8bf27b39053a66af44edc84baecefdc5
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-07-28 15:37:39 -0700

    Made stylistic improvements to the hunt card

--- components/HuntCard.jsx
+++ components/HuntCard.jsx
@@ -1,6 +1,7 @@
 import styles from "../styles/HuntCard.module.css";
 import generateLinkName from "../utility/generateLinkName";
 import { useState } from "react";
+import { updateDoc } from "firebase/firestore";
 
 export default function HuntCard(props) {
     const [encounters, setEncounters] = useState(props.encounters)
@@ -13,10 +14,26 @@ export default function HuntCard(props) {
             </div>
             <img className={styles.image} src={link} alt={props.name} />
             <div className={styles.altBg}>
-                <h2>Encounters:</h2>
-                <h3>{encounters}</h3>
-                <button onClick={() => {setEncounters(encounters + 1)}}>+</button>
+                <h2>Encounters: {encounters}</h2>
+                <div className={styles.buttonLayout}>
+                    <button className={styles.encounterButton} onClick={removeEncounter}>-</button>
+                    <button className={styles.encounterButton} onClick={addEncounter}>+</button>
+                </div>
             </div>
         </div>
     )
+
+    async function removeEncounter() {
+        setEncounters(encounters - 1);
+
+        const newFields = { encounters: encounters - 1 };
+        await updateDoc(props.doc, newFields);
+    }
+
+    async function addEncounter() {
+        setEncounters(encounters + 1);
+
+        const newFields = { encounters: encounters + 1 };
+        await updateDoc(props.doc, newFields);
+    }
 }
