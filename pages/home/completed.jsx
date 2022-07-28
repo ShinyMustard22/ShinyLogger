61cfa5caed993a50575c440ce30506effd97afe3
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-07-28 15:37:26 -0700

    Added a connection to a completed hunts collecting in database

--- pages/home/completed.jsx
+++ pages/home/completed.jsx
@@ -1,9 +1,30 @@
 import HomeLayout from "../../layouts/HomeLayout";
+import HuntCard from "../../components/HuntCard";
+import { db } from "../../firebase/clientApp";
+import { collection, getDocs, doc } from "firebase/firestore";
+import styles from "../../styles/Completed.module.css"
+import { useState, useEffect } from "react";
 
 export default function Completed() {
+    const [huntsData, setHuntsData] = useState([]);
+
+    const huntsRef = collection(db, "completed")
+    useEffect(() => {
+        const getHunts = async () => {
+            const data = await getDocs(huntsRef);
+            setHuntsData(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+        }
+
+        getHunts();
+    }, []);
+
     return (
-        <>
-        </>
+        <div className={styles.main}>
+            {huntsData.map(hunt => {
+                const huntDoc = doc(db, "completed", hunt.id);
+                return <HuntCard name={hunt.name} encounters={hunt.encounters} doc={huntDoc}></HuntCard>
+            })}
+        </div>
     );
 }
 
