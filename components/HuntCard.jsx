a644e6d52150df17232256122ce9e3615c96aa58
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-08-05 16:36:45 -0700

    Changes to saving hunt data

--- components/HuntCard.jsx
+++ components/HuntCard.jsx
@@ -1,34 +1,53 @@
 import styles from "../styles/HuntCard.module.css";
 import generateLinkName from "../utility/generateLinkName";
 import { useState } from "react";
-import { addDoc, updateDoc, deleteDoc } from "firebase/firestore";
+import { updateDoc } from "firebase/firestore";
 import Image from "next/image";
+import DeleteHunt from "./DeleteHunt";
+import CompleteHunt from "./CompleteHunt";
 
 export default function HuntCard(props) {
     const [imageLoaded, setImageLoaded] = useState(false);
+    const [deleteHuntVis, setDeleteHunVis] = useState(false);
+    const [completeHuntVis, setCompleteHuntVis] = useState(false);
 
     const link = `https://img.pokemondb.net/sprites/home/shiny/${generateLinkName(props.name)}.png`;
 
     return (
-       <div className={styles.container}>
+        <>
+        <div className={styles.container}>
             <div className={styles.altBg}>
                 <h1>{props.name}</h1>
-                <button className={styles.cross} onClick={deleteHunt}>✕</button>
-                <button className={styles.check} onClick={completeHunt}>✓</button>
+                <button className={styles.cross} onClick={() => setDeleteHunVis(true)}>✕</button>
+                <button className={styles.check} onClick={() => setCompleteHuntVis(true)}>✓</button>
             </div>
             <div className={styles.imgContainer}>
-                <Image className={styles.image} src={`/api/imageProxy?imageUrl=${link}`}
+                <Image className={styles.image} src={link}
                     layout="fill" onLoad={() => {setImageLoaded(true)}}
                     style={imageLoaded ? {} : {display: 'none'}}></Image>
             </div>
             <div className={styles.altBg}>
-                <h2>Encounters: {props.encounters}</h2>
+                <h1 className={styles.huntNum}>{props.encounters}</h1>
                 <div className={styles.buttonLayout}>
                     <button className={styles.encounterButton} onClick={removeEncounter}>-</button>
                     <button className={styles.encounterButton} onClick={addEncounter}>+</button>
                 </div>
             </div>
         </div>
+        {deleteHuntVis ? 
+            <DeleteHunt 
+                vis={setDeleteHunVis} 
+                doc={props.doc}
+            /> : ""}
+        {completeHuntVis ?
+            <CompleteHunt 
+                vis={setCompleteHuntVis} 
+                doc={props.doc} 
+                completedRef={props.completedRef}
+                name={props.name}
+                encounters={props.encounters}
+                /> : ""}
+        </>
     )
 
     async function removeEncounter() {
@@ -39,14 +58,5 @@ export default function HuntCard(props) {
     async function addEncounter() {
         const newFields = { encounters: props.encounters + 1 };
         await updateDoc(props.doc, newFields);
-    }
-
-    async function deleteHunt() {
-        await deleteDoc(props.doc);
-    }
-
-    async function completeHunt() {
-        deleteHunt();
-        await addDoc(props.completedRef, { name: props.name, encounters: props.encounters });
     }
 }
