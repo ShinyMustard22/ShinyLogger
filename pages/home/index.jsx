76cca4be6fdf03d31bee77ecdb53c7658711da40
Ritam Chakraborty <ritam.chakraborty@gmail.com>
2022-08-10 17:16:42 -0700

    Creating authentication with email and password and using new google sign in button

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,13 +1,25 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { auth } from "../../firebase/clientApp";
-import { signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
-import styles from "../../styles/Home.module.css"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, 
+    signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "firebase/auth";
+import GoogleButton from "react-google-button";
+import styles from "../../styles/Home.module.css";
 
 export default function Home() {
     const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [waitForSignIn, setWaitForSignIn] = useState();
+
     const googleProvider = new GoogleAuthProvider();
 
-    function signIn() {
+    function signUpEmail() {
+        createUserWithEmailAndPassword(auth, email, password)
+        .catch(error => {console.log(error)});
+    }
+
+    function signInGoogle() {
         signInWithPopup(auth, googleProvider)
         .catch(error => {});
     }
@@ -22,7 +34,25 @@ export default function Home() {
             })
         }
         <div className={styles.signInContainer}>
-            <button onClick={signIn} className={styles.googleSignIn}>Continue with Google</button>
+            <div className={styles.signUpContainer}>
+                <h1 className={styles.signUpTitle}>Sign Up</h1>
+                <div className={styles.email}>
+                    <h2 style={{color: 'white'}}>Email</h2>
+                    <input className={styles.emailInput} type="text" placeholder="Email..." onChange={(event) => {setEmail(event.target.value)}}/>
+                </div>
+                <div className={styles.email}>
+                    <h2 style={{color: 'white'}}>Password</h2>
+                    <input className={styles.emailInput} type="password" placeholder="Password..." onChange={(event) => {setPassword(event.target.value)}}/>
+                </div>
+                <button className={styles.signUp} onClick={signUpEmail}>Continue</button>
+            </div>
+            <div className={styles.altSignUpContainer}>
+                <GoogleButton 
+                    onClick={signInGoogle} 
+                    label="Continue with Google" 
+                    disabled={waitForSignIn}
+                />
+            </div>
         </div>
         </>
     );
